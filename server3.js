c3c982dfaeb2409e65e400b60022105055906a91
Vspecc <lienneseb@gmail.com>
2025-08-08 12:30:03 +0800

    Create server3.js

---
+++ server3.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+// This responds with 'Hello World' on the homepage
+app.get('/', function(req, res) {
+  console.log('There is GET request for the homepage!');
+  res.send('Here is the GET Method!');
+})
+
+// This responds a POST request for the homepage
+app.post('/', function(req, res) {
+  console.log('A POST request for the homepage is accessed.');
+  res.send('Here is the POST Method!');
+})
+
+// This responds a GET request for the /list_user page
+app.get('/cunanan_lienne', function(req, res) {
+  console.log('Got a GET request for /cunanan_lienne');
+  res.send('I am IT student and my department is school of computing');
+})
+
+// This responds a GET request for /abced, abced, ab123cd, and so on
+app.get('/abced', function(req, res) {
+  console.log('Got a GET request for /abced');
+  res.send('Pattern Match Page!');
+})
+
+var server = app.listen(4000, function() {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Example app listening at http://%s:%s', host, port);
+})
